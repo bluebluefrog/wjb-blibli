b8ef48c95ea77ddc9e6908d5246c7384fb91cba0
jingbin.wang <79555784+bluebluefrog@users.noreply.github.com>
2022-06-18 15:17:35 +1000

    feat-mapperScan

--- wjb-blibli-controller/src/main/java/com/BlibliApplicationApp.java
+++ wjb-blibli-controller/src/main/java/com/BlibliApplicationApp.java
@@ -1,9 +1,11 @@
 package com;
 
 import com.wjb.blibli.websocket.WebSocketService;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement//开启事务
 @EnableAsync//开启异步
 @EnableScheduling//开启定时任务
+@MapperScan(basePackages = "com.wjb.blibli.dao")
 public class BlibliApplicationApp {
 
     public static void main(String[] args) {
